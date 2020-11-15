c6c80c10ce7347c0437f540ab7a6654bb450480e
Lucas Mendonca <bragalucas@id.uff.br>
2020-11-15 06:08:18 +0000

    libtree

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import	ply.lex as lex
 from utils import slurp
+from treelib import Node, Tree
+import json
 
+class Tests(dict):
+    def __init__(self, testNo, parentNo, status = "", comment = ""):
+        dict.__init__(self, testNo=testNo, parentNo=parentNo, status=status, comment = comment)
+        self.testNo = testNo
+        self.parentNo = parentNo
+        self.status = status
+        self.comment = comment
+        self.teste = str(testNo) + " " + str(parentNo) + " " + str(status) + " " + str(comment)
+
+tree_aux = None
 n_tests = 0 #quantidade total de testes
 n_ok_tests = 0 #quantidade total de ok testes
 n_nok_tests = 0 #quantidade total de not ok testes
@@ -12,6 +24,12 @@ n_nok_subtests = 0 #quantidade total de not ok subtestes
 tokens = ('N_TESTS', 'OK_TEST', 'NOK_TEST', 'N_SUBTESTS', 'OK_SUBTEST', 'NOK_SUBTEST', 'COMMENT')
 
 t_ignore = "\n"
+
+resultTree = Tree()
+node_root = resultTree.create_node(tag = "Root", identifier= "root")
+
+def getNivel(s):
+    return int((len(s) - len(s.lstrip()))/4)
 
 def t_error(t):
     #print("Illegal character '%s'" % t.value[0])
@@ -37,13 +55,30 @@ def t_N_SUBTESTS(t):
 def t_OK_TEST(t):
     r"(ok)\s[0-9]+\s"
     global n_ok_tests
+    global nodes, node, tree_aux
+    nodes = [None] * 100
+
+    if tree_aux != None:
+        node = resultTree.create_node(t.value, t.value, parent= node_root, data = Tests(1, 0, "OK", t.value))
+        resultTree.merge(t.value, tree_aux)
+    else:
+        resultTree.create_node(t.value, t.value, parent= node_root, data = Tests(1, 0, "OK", t.value)) 
     n_ok_tests += 1
     return t
 
 def t_OK_SUBTEST(t):
     r"\s+(ok)\s[0-9]+\s"
-    global n_ok_subtests
+    global n_ok_subtests, tree_aux
     n_ok_subtests += 1
+    node = None
+    n = getNivel(t.value) 
+    if n == 1:
+        if tree_aux != None:
+            node = tree_aux.get_node("aux")
+        if node == None:
+            tree_aux = Tree()
+            nodes[n-1] = tree_aux.create_node("aux", "aux", data = Tests(1, 0, "OK", "aux"))
+    nodes[n] = tree_aux.create_node(t.value, t.value, parent=nodes[n-1], data = Tests(1, 0, "OK", t.value)) 
     return t
 
 def t_NOK_TEST(t):
@@ -72,3 +107,12 @@ print("NOT OK testes: %d" % n_nok_tests)
 print("Total subtestes: %d " % n_subtests)
 print("OK subtestes: %d" % n_ok_subtests)
 print("NOT OK subtestes: %d" % n_nok_subtests)
+
+resultTree.show(line_type="ascii-em")
+#resultTree.save2file('tree.txt')
+print(resultTree.to_json(with_data=True))
+
+data = resultTree.to_json(with_data=True)
+
+with open('data.json', 'w') as outfile:
+    outfile.write(data)
